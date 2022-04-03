d0cd92c63debad9f7a8fef67d1b7a5cf3533a9c4
MarselDav <95362733+MarselDav@users.noreply.github.com>
2022-04-03 19:16:48 +0300

    Update first.py

--- first.py
+++ first.py
@@ -1,6 +1,21 @@
-def main():
-    print("My first git-repo")
+import os
+
+from PIL import Image
+from flask import Flask, render_template, request
+
+app = Flask(__name__)
 
 
-if __name__ == "__main__":
-    main()
+@app.route("/galery", methods=["POST", "GET"])
+def images_galery():
+    images = ["img/" + i for i in os.listdir("static/img")[1::]]
+    if request.method == "POST":
+        file = request.files['file']
+        image = Image.open(file)
+        image.save("static/img/photo" + str(len(images) + 1) + ".png")
+    return render_template("index.html", images=images, len_=(len(images) + 1))
+
+
+if __name__ == '__main__':
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host='0.0.0.0', port=port)
