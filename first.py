5b9695f0c782abf77db356537c199f796a552d7d
MarselDav <95362733+MarselDav@users.noreply.github.com>
2022-04-03 19:45:49 +0300

    Update first.py

--- first.py
+++ first.py
@@ -1,13 +1,19 @@
 import os
 
-from flask import Flask
+from PIL import Image
+from flask import Flask, render_template, request
 
 app = Flask(__name__)
 
 
-@app.route("/")
-def index():
-    return "Привет от приложения Flask"
+@app.route("/galery", methods=["POST", "GET"])
+def images_galery():
+    images = ["img/" + i for i in os.listdir("static/img")[1::]]
+    if request.method == "POST":
+        file = request.files['file']
+        image = Image.open(file)
+        image.save("static/img/photo" + str(len(images) + 1) + ".png")
+    return render_template("index.html", images=images, len_=(len(images) + 1))
 
 
 if __name__ == '__main__':
