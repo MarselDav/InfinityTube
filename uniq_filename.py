8337fe20559fcb600df371fc8a0a0573fc2a9f80
MarselDav <95362733+MarselDav@users.noreply.github.com>
2022-04-21 14:13:33 +0300

    Delete uniq_filename.py

--- uniq_filename.py
+++
@@ -1,7 +0,0 @@
-import datetime
-
-
-def get_uniq_filename():  # получить уникальное название файла
-    uniq_filename = str(datetime.datetime.now().date()) + '_' \
-                    + str(datetime.datetime.now().time()).replace(':', '.') + ".mp4"
-    return uniq_filename
